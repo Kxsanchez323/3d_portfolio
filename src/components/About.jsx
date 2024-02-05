95b0b4d4485996d6247a98cdc01cb5ff8361e8ce
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-02-05 14:04:08 -0500

    Changed About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,57 +1,66 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
-import { motion } from 'framer-motion';
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant} from '../utils/motion'
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
-import { SectionWrapper } from '../hoc';
+import { SectionWrapper } from "../hoc";
 
-const ServiceCard = ({ index, title, icon}) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className='xs:w-[250px] w-full'>
+    <Tilt className="xs:w-[250px] w-full">
       <motion.div
-      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <div 
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
-  )
-}
+  );
+};
 
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview.</h2>
-    </motion.div>
-    
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-    <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-    I'm a 23 year-old Dominican frontend software engineer with a strong passion for developing websites with great user experiences.
-I currently work on expanding my knowledge of tech stacks following industry best standards, with the goal of creating web applications that positively impact the world.
-    </motion.p>
-    
-    <div className='mt-20 flex flex-wrap gap-10'>
-      {services.map((service, index) => (
-        <ServiceCard key={service.title} index={index} {...service}/>
-      ))}
-    </div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a 23 year-old Dominican software engineer with a strong passion for
+        developing web applications with great user experiences. I'm currently
+        looking for roles as a frontend developer or full stack developer. My
+        day to day consists of working on expanding my knowledge of tech stacks
+        following industry best standards, with the goal of creating web
+        applications that positively impact the world. I'm an advid gamer and I
+        one day dream of being able to work at SEGA.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About, "about")
+export default SectionWrapper(About, "about");
