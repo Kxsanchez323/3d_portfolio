8c4f7e4fd29180245c8bf0dff509fb07f33fdf37
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-02-06 02:08:26 -0500

    Adjusted 3d models

--- src/constants/index.js
+++ src/constants/index.js
@@ -110,14 +110,7 @@ const technologies = [
     name: "git",
     icon: git,
   },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
+  
 ];
 
 const experiences = [
